7aa2285b708a572b3f45165acf742a088d668d45
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2022-09-09 00:52:24 +0530

    Add files via upload

---
+++ classshow.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class show{
+
+    int a=10;
+public :
+    void show1(){
+    cout<<a;
+    }
+
+};
+int main()
+{
+    show obj;
+    obj.show1();
+    return 0;
+}
